64d1217853a03c9440e6a04431dd8efd5eba3653
aryanabuelna <aryanabuelna0510@gmail.com>
2023-03-21 23:13:30 -0700

    Update NewStudent.js

--- src/components/NewStudent.js
+++ src/components/NewStudent.js
@@ -1,4 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import {DataGrid} from '@mui/x-data-grid';
+import {SEMESTER_LIST} from '../constants.js'
+
 class NewStudent extends Component {
+    render() {
+        <div>
+            <Button variant="outlined" color="primary" style={{margin: 10}}>
+                Add New Student
+            </Button>
+        </div>
+    };
 };
 
 export default NewStudent;
